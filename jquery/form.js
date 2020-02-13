0bf212f798bcb07f360979ce58c4840f7043aebb
Austin Denny <austinbdenny@gmail.com>
2020-02-13 11:57:53 -0600

    lecture up to lunch break.

---
+++ jquery/form.js
@@ -0,0 +1,13 @@
+// let myFunction = () => {
+//   alert('form was subimtted');
+// };
+
+$(() => {
+  //   $('#myform').submit(e => {
+  //     alert('form was submitted ');
+  //   });
+  //   $('myform').submit(function(e) {
+  //     e.preventDefault();
+  //     $('input:checked').val();
+  //   });
+});
