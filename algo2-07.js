f742d0eb8d2d0bd490df0812174a3e4d6ec9939d
Austin Denny <austinbdenny@gmail.com>
2020-02-14 13:21:19 -0600

    Algo friday.

--- algo2-07.js
+++ algo2-07.js
@@ -86,3 +86,17 @@
 //   return largest;
 // }
 // largestPalindrome();
+
+////////FizzBuzz
+
+for (let index = 1; index < 101; index++) {
+  if (index % 3 == 0 && index % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (index % 5 == 0) {
+    console.log('buzz');
+  } else if (index % 3 == 0) {
+    console.log('fizz');
+  } else {
+    console.log(index);
+  }
+}
