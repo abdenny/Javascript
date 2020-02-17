4ca4eea6f8c2192029128a7e0240c77ab25f01ba
Austin Denny <austinbdenny@gmail.com>
2020-02-17 11:35:31 -0600

    Lecture up to lunch.

---
+++ AJAX_JSON_API/notes1.js
@@ -0,0 +1,22 @@
+///////// AJAX
+//Stands for Async JS and XML
+//Allows us to change page without reloading it. XHR is the tech behind AJAX.
+
+//FETCH IN JS
+//Not actually in JS, in the browser.
+
+// console.log('comment 1 ');
+
+// console.log('comment 3');
+
+// setTimeout(() => {
+//   console.log('comment 2');
+// }, 1000);
+
+////fetch(url)
+// .then((response))=>{
+//     response.json /////convert string into Object
+// }
+// .then(jsonObjectWeJustConverted) =>{
+// we do magic with it
+// }
